50cb5e3e1e6b5ac9350ec6a9475ca0ce2af6b5bb
sgg2123 <sgg2123@columbia.edu>
2018-07-13 10:51:52 -0400

    moved form state to app in order to update cocktail list upon submit

--- src/App.js
+++ src/App.js
@@ -12,6 +12,16 @@ class App extends Component {
     filteredCocktails: [],
     searchTerm: "",
     selectCocktail: null,
+    name: "",
+    description: "",
+    instructions: "",
+    proportions: [
+      {
+        id: 0,
+        ingredient_name: "",
+        amount: "",
+      }
+    ],
   }
 
   componentDidMount() {
@@ -42,6 +52,101 @@ class App extends Component {
     });
   }
 
+  handleChange = (event) => {
+    if (event.target.name === 'amount' || event.target.name === 'ingredient_name') {
+      let proportionsCopy = [...this.state.proportions]
+      let newProportions = proportionsCopy.map(prop => {
+        if (prop.id === parseInt(event.target.id, 10)) {
+          prop[event.target.name] = event.target.value
+        }
+        return prop
+      })
+      this.setState({
+        proportions: newProportions
+      })
+    } else {
+      this.setState({
+        [event.target.name]: event.target.value
+      })
+    }
+  }
+
+  handleAddIngredient = () => {
+    this.setState({
+      proportions: this.state.proportions.concat([{id: this.state.proportions.length, ingredient_name: '', amount: ''}])
+    });
+  }
+
+  handleFormSubmit = (event) => {
+    event.preventDefault()
+    const name = this.state.name.toUpperCase()
+    const description = this.state.description
+    const instructions = this.state.instructions
+    const proportions = this.state.proportions
+    const source = 'User Input'
+
+    let body = {name: name, description: description, instructions: instructions, source: source};
+    let config = {
+      method: 'POST',
+      headers: {"Content-type":"application/json"},
+      body: JSON.stringify(body)
+    };
+
+    fetch('http://localhost:3000/api/v1/cocktails', config)
+    .then(r => r.json())
+    .then(data => {
+      if (data.error) {
+        alert(data.error);
+        return;
+      }
+      fetch('http://localhost:3000/api/v1/cocktails')
+      .then(r => r.json())
+      .then(data => {
+
+        let foundCocktail = data.find(cocktail => cocktail.name === name)
+        let id = parseInt(foundCocktail.id, 10)
+
+        let newArr = []
+        proportions.forEach(prop => {
+          newArr.push({ingredient_name: prop.ingredient_name, amount: prop.amount})
+        })
+
+        let body = {proportions: newArr};
+        let config = {
+          method: 'PATCH',
+          headers: {"Content-type":"application/json"},
+          body: JSON.stringify(body)
+        };
+
+        fetch(`http://localhost:3000/api/v1/cocktails/${id}`, config)
+        .then(this.setState(
+          {
+            name: "",
+            description: "",
+            instructions: "",
+            proportions: [
+              {
+                id: 0,
+                ingredient_name: "",
+                amount: "",
+              }
+            ],
+          }, () => {
+            fetch('http://localhost:3000/api/v1/cocktails')
+            .then(r => r.json())
+            .then(data => {
+              this.setState({
+                cocktails: data,
+                filteredCocktails: data,
+              });
+            });
+          }
+        ))
+      })
+    })
+    alert('Cocktail Created!')
+  };
+
   render() {
     return (
       <div className="App">
@@ -55,7 +160,15 @@ class App extends Component {
         }
         {/*<List cocktails={this.filteredCocktails()} select={this.selectCocktail} />*/}
         <List cocktails={this.state.filteredCocktails} selectCocktail={this.selectCocktail} />
-        <CreateForm />
+        <CreateForm
+          handleFormSubmit={this.handleFormSubmit}
+          handleChange={this.handleChange}
+          handleAddIngredient={this.handleAddIngredient}
+          name={this.state.name}
+          description={this.state.description}
+          instructions={this.state.instructions}
+          proportions={this.state.proportions}
+        />
       </div>
     );
   }
--- src/CreateForm.js
+++ src/CreateForm.js
@@ -1,143 +1,40 @@
 import React from 'react';
 
 class CreateForm extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      name: "",
-      description: "",
-      instructions: "",
-      proportions: [
-        {
-          id: 0,
-          ingredient_name: "",
-          amount: "",
-        }
-      ],
-    };
-  }
-
-  handleChange = (event) => {
-    if (event.target.name === 'amount' || event.target.name === 'ingredient_name') {
-      let proportionsCopy = [...this.state.proportions]
-      let newProportions = proportionsCopy.map(prop => {
-        if (prop.id === parseInt(event.target.id, 10)) {
-          prop[event.target.name] = event.target.value
-        }
-        return prop
-      })
-      this.setState({
-        proportions: newProportions
-      })
-    } else {
-      this.setState({
-        [event.target.name]: event.target.value
-      })
-    }
-  }
-
-  handleAddIngredient = () => {
-    this.setState({
-      proportions: this.state.proportions.concat([{id: this.state.proportions.length, ingredient_name: '', amount: ''}])
-    });
-  }
-
-  handleFormSubmit = (event) => {
-    event.preventDefault()
-    const name = this.state.name.toUpperCase()
-    const description = this.state.description
-    const instructions = this.state.instructions
-    const proportions = this.state.proportions
-    const source = 'User Input'
-
-    let body = {name: name, description: description, instructions: instructions, source: source};
-    let config = {
-      method: 'POST',
-      headers: {"Content-type":"application/json"},
-      body: JSON.stringify(body)
-    };
-
-    fetch('http://localhost:3000/api/v1/cocktails', config)
-    .then(r => r.json())
-    .then(data => {
-      if (data.error) {
-        alert(data.error);
-        return;
-      }
-      fetch('http://localhost:3000/api/v1/cocktails')
-      .then(r => r.json())
-      .then(data => {
-
-        let foundCocktail = data.find(cocktail => cocktail.name === name)
-        let id = parseInt(foundCocktail.id, 10)
-
-        let newArr = []
-        proportions.forEach(prop => {
-          newArr.push({ingredient_name: prop.ingredient_name, amount: prop.amount})
-        })
-
-        let body = {proportions: newArr};
-        let config = {
-          method: 'PATCH',
-          headers: {"Content-type":"application/json"},
-          body: JSON.stringify(body)
-        };
-
-        fetch(`http://localhost:3000/api/v1/cocktails/${id}`, config)
-        .then(this.setState(
-          {
-            name: "",
-            description: "",
-            instructions: "",
-            proportions: [
-              {
-                id: 0,
-                ingredient_name: "",
-                amount: "",
-              }
-            ],
-          }
-        ))
-      })
-    })
-    alert('Cocktail Created!')
-  };
-
   render() {
     return (
       <div className="form">
 
         <h1>Create a Cocktail</h1>
 
-        <form onSubmit={this.handleFormSubmit}>
+        <form onSubmit={this.props.handleFormSubmit}>
           <label>
             Name
-            <input type="text" name="name" className="form-input" value={this.state.name} onChange={this.handleChange} />
+            <input type="text" name="name" className="form-input" value={this.props.name} onChange={this.props.handleChange} />
           </label>
 
           <label>
             Description
-            <input type="text" name="description" className="form-input" value={this.state.description} onChange={this.handleChange} />
+            <input type="text" name="description" className="form-input" value={this.props.description} onChange={this.props.handleChange} />
           </label>
 
           <label>
             Instructions
-            <input type="text" name="instructions" className="form-input" value={this.state.instructions} onChange={this.handleChange} />
+            <input type="text" name="instructions" className="form-input" value={this.props.instructions} onChange={this.props.handleChange} />
           </label>
 
           <h4>Proportions</h4>
-          {this.state.proportions.map(proportion => {
+          {this.props.proportions.map(proportion => {
             return (
               <div key={proportion.id}>
                 <label>
                   Ingredient Name
-                  <input id={proportion.id} key={`ingredient-${proportion.id}`} type="text" name="ingredient_name" className="form-input" value={this.state.proportions[proportion.id].ingredient_name} onChange={this.handleChange} />
+                  <input id={proportion.id} key={`ingredient-${proportion.id}`} type="text" name="ingredient_name" className="form-input" value={this.props.proportions[proportion.id].ingredient_name} onChange={this.props.handleChange} />
                 </label>
 
                 <label>
                   Amount
-                  <input id={proportion.id} key={`amount-${proportion.id}`} type="text" name="amount" className="form-input" value={this.state.proportions[proportion.id].amount} onChange={this.handleChange} />
+                  <input id={proportion.id} key={`amount-${proportion.id}`} type="text" name="amount" className="form-input" value={this.props.proportions[proportion.id].amount} onChange={this.props.handleChange} />
                 </label>
                 <p> </p>
               </div>
@@ -147,7 +44,7 @@ class CreateForm extends React.Component {
           <div>
             <p> </p>
             <p> </p>
-            <button type="button" onClick={this.handleAddIngredient}>Add Ingredient</button>
+            <button type="button" onClick={this.props.handleAddIngredient}>Add Ingredient</button>
             <p> </p>
           </div>
 
